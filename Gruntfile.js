f61786fd65cf26dc621e12795b5edf5987e11891
futurekind <f.trinler@gmail.com>
2013-10-10 22:29:23 +0200

    Changing structure to grunt-jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,136 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-compass');
+
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-jekyll');
 
 	grunt.initConfig({
-		compass: {
-			options: {
-				sassDir: 'scss',
-				cssDir: 'css',
-				imagesDir: '',
-				javascriptsDir: 'js',
-				relativeAssets: true
-			},
 
-			dev: {
-				options: {
-					outputStyle: 'expanded'
-				}
-			},
-
-			build: {
-				options: {
-					outputStyle: 'compressed',
-					force: true
+		sass: {
+			all: {
+				files: {
+					'css/dist/main.css': 'css/src/main.scss',
 				}
 			}
 		},
 
+		autoprefixer: {
+			dist: {
+				files: {
+					'css/dist/main.css': 'css/dist/main.css'
+				}
+			}
+		},
+
+		cssmin: {
+			minify: {
+				files: {
+					'css/dist/main.css': 'css/dist/main.css'
+				}
+			}
+		},
+
+		jshint: {
+			files: [
+				'Gruntfile.js',
+				'js/src/**/*.js'
+			]
+		},
+
+		concat: {
+			js: {
+				src: ['js/src/modules/*.js', 'js/src/*.js'],
+				dest: 'js/dist/main.js'
+			},
+
+			vendor: {
+				src: ['js/vendor/**/*.js'],
+				dest: 'js/dist/libs.js'
+			},
+
+			all: {
+				src: ['js/dist/libs.js', 'js/dist/main.js'],
+				dest: 'js/dist/app.js'
+			}
+		},
+
+		uglify: {
+			all: {
+				files: {
+					'js/dist/app.js': 'js/dist/app.js'
+				}
+			}
+		},
+
+		jekyll: {
+			serve: true
+		},
+
 		watch: {
-			sass: {
-				files: ['scss/**/*.scss'],
-				tasks: ['compass:dev'],
+
+			js: {
+				files: '<%= jshint.files %>',
+				tasks: ['jshint', 'concat:js', 'concat:all'],
 				options: {
-					livereload: 35729
+					livereload: true
+				}
+			},
+
+			vendor: {
+				files: '<%= concat.vendor.src %>',
+				tasks: ['concat:vendor', 'concat:all']
+			},
+
+			scss: {
+				files: 'css/src/**/*.scss',
+				tasks: ['sass', 'autoprefixer', 'jekyll'],
+				options: {
+					livereload: true
+				}
+			},
+
+			html: {
+				files: ['_includes/*.*', '_layouts/*.*', '_posts/*.*', '*.html'],
+				tasks: ['jekyll'],
+				options: {
+					livereload: true
 				}
 			}
 		}
 
 	});
+
+	function createJsModule(data) {
+		var tpl = grunt.file.read('js/src/modules/module.tpl'),
+			out = grunt.template.process(tpl, {data: data});
+
+		grunt.file.write('js/src/modules/' + data.name + '.js', out);
+
+		grunt.log.writeln('Created js/src/modules/' + data.name + '.js');
+	}
+
+
+	grunt.registerTask('js-module', 'Creating module for js', function(moduleName){
+		var data;
+
+		if (arguments.length === 0) {
+			data = {
+				name: 'MyModule'
+			};
+		} else {
+			data = {
+				name: moduleName
+			};
+		}
+
+		createJsModule(data);
+	});
+
+	grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'sass', 'autoprefixer', 'cssmin']);
+	grunt.registerTask('init', ['concat', 'sass', 'autoprefixer']);
 };
